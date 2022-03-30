8ce56d352cf92b8d7a4f9c4e43658caf47b339f1
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-03-30 15:27:04 -0300

    ejs accesories y coffee

--- src/controller/products.js
+++ src/controller/products.js
@@ -1,9 +1,13 @@
+const accesories = require('../data/accesories.json');
+const coffee = require('../data/coffee.json');
 
 module.exports = {
     coffee: (req, res) => res.render('headerLinks/coffee',{
+        coffee: coffee,
         styles: ['headerLinks/coffee']
     }),
     accesories: (req, res) => res.render('headerLinks/accesories',{
+        accesories: accesories,
         styles: ['headerLinks/accesories']
     })
 }
