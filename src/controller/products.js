22421b5cd5649f97c9ca432ab965045cedc0df65
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-08 13:04:08 -0300

    ajutando pagina detalle

--- src/controller/products.js
+++ src/controller/products.js
@@ -5,8 +5,12 @@ module.exports = {
         products: products,
         styles: 'categories'
     }),
-    products: (req,res) => res.render('headerLinks/products', {
-        products: products,
-        styles: 'products'
-    })
+    detail: (req,res) => { 
+        let product = products.find(product => product.id === req.params.id)
+        //res.send(product);
+        res.render('headerLinks/detail', {
+            product: product,
+            styles: 'detail'
+        }
+    )}
 }
