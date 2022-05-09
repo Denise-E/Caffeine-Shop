5c4ce6bd83f2407c39231a2af3fb8053c733d9c4
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-09 12:24:15 -0300

    Subiendo ajustes

--- src/controller/products.js
+++ src/controller/products.js
@@ -2,15 +2,7 @@ const products = require('../data/products.json');
 
 module.exports = {
     categories: (req,res) => res.render('headerLinks/categories', {
-        products: filteredP,
+        products: products,
         styles: 'categories'
-    }),
-    detail: (req,res) => { 
-        let product = products.find(product => product.id === req.params.id)
-        //res.send(product);
-        res.render('headerLinks/detail', {
-            product: product,
-            styles: 'detail'
-        }
-    )}
+    })
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,6 @@ const products = require('../controller/products.js');
 const express = require('express');
 const router = express.Router();
 
-router.get('/:id', products.detail)
 router.get('/categories/:id', products.categories)
 
 
