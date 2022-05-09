25a14cc24fea5ccc986a3d2635a60b53c59966d3
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-09 12:56:31 -0300

    Subiendo página categorias

--- src/controller/products.js
+++ src/controller/products.js
@@ -1,8 +1,13 @@
 const products = require('../data/products.json');
+const categories = require('../data/categories.json');
 
 module.exports = {
-    categories: (req,res) => res.render('headerLinks/categories', {
-        products: products,
-        styles: 'categories'
-    })
+    categories: (req,res) => {
+        let result = categories.find(element => element.id == req.params.id);
+        res.render('headerLinks/categories', {
+            category: result,
+            products: products,
+            styles: 'categories'
+        })
+    }
 }
